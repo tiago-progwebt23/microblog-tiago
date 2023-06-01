61adee834dce6f9e3a46c7bd873d443e376fc540
tiago-progwebt23 <tiago.progwebt23@gmail.com>
2023-06-01 14:08:58 -0300

    Update funcoes-noticias.php

--- inc/funcoes-noticias.php
+++ inc/funcoes-noticias.php
@@ -132,6 +132,23 @@ function atualizarNoticia($conexao, $titulo, $texto, $resumo,
 } // fim atualizarNoticia
 
 
+/* Usada em noticia-exclui.php */
+function excluirNoticia($conexao, $idNoticia, $idUsuarioLogado, 
+                        $tipoUsuarioLogado){
+
+    if($tipoUsuarioLogado == 'admin'){
+        /* SQL do admin: pode apagar qualquer notícia pelo id */
+        $sql = "DELETE FROM noticias WHERE id = $idNoticia";
+    } else {
+        /* SQL do editor: pode apagar SOMENTE suas próprias notícias
+        (pelo id da notícia e pelo seu próprio id) */
+        $sql = "DELETE FROM noticias WHERE id = $idNoticia
+                AND usuario_id = $idUsuarioLogado";
+    }     
+    
+    mysqli_query($conexao, $sql) or die(mysqli_error($conexao));
+
+} // fim excluirNoticia
 
 
 
