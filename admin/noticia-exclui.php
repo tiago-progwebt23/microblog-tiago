e4061595735e849b9a2254d517a346c6a01f50dd
tiago-progwebt23 <tiago.progwebt23@gmail.com>
2023-06-01 14:34:49 -0300

    Update noticia-exclui.php

--- admin/noticia-exclui.php
+++ admin/noticia-exclui.php
@@ -1,3 +1,20 @@
 <?php
+require_once "../inc/funcoes-noticias.php";
 require_once "../inc/funcoes-sessao.php";
 verificaAcesso();
+
+// Pegando o id da notícia vindo do parâmetro de URL
+$idNoticia = $_GET['id'];
+
+// Pegando o id e o tipo do usuário logado vindos da SESSION
+$idUsuario = $_SESSION['id'];
+$tipoUsuario = $_SESSION['tipo'];
+
+// Executando a função de DELETE com os parâmetros
+excluirNoticia($conexao, $idNoticia, $idUsuario, $tipoUsuario);
+
+// Voltando pra páginas das notícias
+header("location:noticias.php");
+
+
+
