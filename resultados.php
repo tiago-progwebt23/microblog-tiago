7dd965df77d56fc2f2d2eba017cbd6e8014882ac
tiago-progwebt23 <tiago.progwebt23@gmail.com>
2023-06-07 14:49:00 -0300

    Término da busca

--- resultados.php
+++ resultados.php
@@ -4,43 +4,36 @@ require "inc/cabecalho.php";
 
 $termo = $_GET['busca'];
 $resultadoDaBusca = busca($conexao, $termo);
-var_dump($resultadoDaBusca);
+//var_dump($resultadoDaBusca);
 ?>
 
 
 <div class="row bg-white rounded shadow my-1 py-4">
     <h2 class="col-12 fw-light">
-        Você procurou por <span class="badge bg-dark">termo buscado</span> e
-        obteve <span class="badge bg-info">X</span> resultados
+        Você procurou por <span class="badge bg-dark">
+             <?=$termo?> </span> e
+        obteve 
+        <span class="badge bg-dark"> <?=count($resultadoDaBusca)?> </span> resultados
     </h2>
     
-    <div class="col-12 my-1">
-        <article class="card">
-            <div class="card-body">
-                <h3 class="fs-4 card-title fw-light">Título da notícia...</h3>
-                <p class="card-text">
-                    <time>Data da notícia</time> - 
-                    Resumo da notícia.
-                </p>
-                
-                <a href="noticia.php" class="btn btn-primary btn-sm">Continuar lendo</a>
-            </div>
-        </article>
-    </div>
 
+<?php foreach($resultadoDaBusca as $resultado) { ?>    
     <div class="col-12 my-1">
         <article class="card">
             <div class="card-body">
-                <h3 class="fs-4 card-title fw-light">Título da notícia...</h3>
+                <h3 class="fs-4 card-title fw-light">
+                    <?=$resultado['titulo']?>
+                </h3>
                 <p class="card-text">
-                    <time>Data da notícia</time> - 
-                    Resumo da notícia.
+                    <time> <?=formataData($resultado['data'])?> </time> - 
+                    <?=$resultado['resumo']?>
                 </p>
                 
-                <a href="noticia.php" class="btn btn-primary btn-sm">Continuar lendo</a>
+                <a href="noticia.php?id=<?=$resultado['id']?>" class="btn btn-primary btn-sm">Continuar lendo</a>
             </div>
         </article>
     </div>
+<?php } ?>    
 
 
 </div>     
